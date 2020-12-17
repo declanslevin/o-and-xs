e4e46f636f224385479741f06ad7866ede433166
declanslevin <33623842+declanslevin@users.noreply.github.com>
2020-12-17 12:51:17 +0000

    wip

--- src/main.js
+++ src/main.js
@@ -5,45 +5,26 @@
 // 3. Add ability to play 2 player
 // 4. Make CPU smarter? Add difficulty levels (Impossible/Normal/Stupid)?
 
-const play = require("../lib/play.js");
+const { store } = require("../lib/store");
+const { play } = require("../lib/play");
 
-const store = {
-  grid: {
-    1: 1,
-    2: 2,
-    3: 3,
-    4: 4,
-    5: 5,
-    6: 6,
-    7: 7,
-    8: 8,
-    9: 9,
-  },
-  userTeam: null,
-  cpuTeam: null,
-  nextPlayer: null,
-  choices: [],
-  winner: false,
-};
-
-const storeTest = {
-  grid: {
-    1: "O",
-    2: "O",
-    3: "X",
-    4: "X",
-    5: 5,
-    6: "O",
-    7: "O",
-    8: "X",
-    9: "X",
-  },
-  userTeam: null,
-  cpuTeam: null,
-  nextPlayer: null,
-  choices: [],
-  winner: false,
-};
+// const store = {
+//   grid: {
+//     1: 1,
+//     2: 2,
+//     3: 3,
+//     4: 4,
+//     5: 5,
+//     6: 6,
+//     7: 7,
+//     8: 8,
+//     9: 9,
+//   },
+//   userTeam: null,
+//   cpuTeam: null,
+//   nextPlayer: null,
+//   choices: [],
+//   winner: false,
+// };
 
 play(store);
-// play(storeTest);
