2ca42a6aeb59d3204fee3e3489c78fb2a2123a8e
declanslevin <33623842+declanslevin@users.noreply.github.com>
2021-01-06 17:30:45 +0000

    Create Store class, update tests

--- src/main.js
+++ src/main.js
@@ -5,26 +5,8 @@
 // 3. Add ability to play 2 player
 // 4. Make CPU smarter? Add difficulty levels (Impossible/Normal/Stupid)?
 
-const { store } = require("../lib/store");
+const { Store } = require("../lib/store");
 const { play } = require("../lib/play");
 
-// const store = {
-//   grid: {
-//     1: 1,
-//     2: 2,
-//     3: 3,
-//     4: 4,
-//     5: 5,
-//     6: 6,
-//     7: 7,
-//     8: 8,
-//     9: 9,
-//   },
-//   userTeam: null,
-//   cpuTeam: null,
-//   nextPlayer: null,
-//   choices: [],
-//   winner: false,
-// };
-
+const store = new Store();
 play(store);
