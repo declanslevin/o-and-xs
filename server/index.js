fb7c77a48f5809c9e4c1b4bcf4376046456fc312
declanslevin <33623842+declanslevin@users.noreply.github.com>
2021-01-19 15:06:57 +0000

    ALL THE HAX

--- server/index.js
+++ server/index.js
@@ -1,30 +1,38 @@
 const WebSocket = require("ws");
+const rl = require("../lib/rl");
 
+console.log("*****************");
+console.log("STARTING");
 const wss = new WebSocket.Server({ port: 8080 });
-// const wss = new WebSocket.Server({ port: 8082, noServer: true });
 
-// const clients = new Set();
+const promptForTeamChoice = () => {
+  return new Promise((resolve) => {
+    rl.question("Pick your team. O or X? = ", (choice) => {
+      return resolve(choice.toUpperCase());
+    });
+  });
+};
 
-// http.createServer((req, res) => {
-//   wss.handleUpgrade(req, req.socket, Buffer.alloc(0), onSocketConnect);
-// });
-
-// const onSocketConnect = (ws) => {
-//   clients.add(ws);
-
-//   ws.on("connection", () => {
-//     console.log("New client connected!");
-//   });
-//   ws.on("close", () => {
-//     console.log("Client has disconnected");
-//   });
-// };
-wss.on("upgrade", (req, socket, head) => {
-  wss.handleUpgrade(req, socket, head);
-});
-
-wss.on("connection", (ws) => {
+wss.on("connection", async (ws) => {
   console.log("New client connected!");
+  const question = (prompt, cb) => {
+    console.log(prompt);
+    ws.send(prompt);
+    // TODO unregister cb
+    // ws.on("message", cb);
+    ws.onmessage = ({ data }) => {
+      console.log("DATA = " + data);
+      cb(data);
+      ws.onmessage = null;
+    };
+  };
+  rl.question = question;
+  const result = await promptForTeamChoice();
+  console.log("RESULT = " + result);
+
+  // ws.on("message", (data) => {
+  //   console.log(`Client has sent us: ${data}`);
+  // });
 
   ws.on("close", () => {
     console.log("Client has disconnected");
