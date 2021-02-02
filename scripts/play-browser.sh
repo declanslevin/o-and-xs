80dc3f33fa67d04aac46ae8cc3e8c520862bfff6
declanslevin <33623842+declanslevin@users.noreply.github.com>
2021-02-02 11:34:15 +0000

    update scripts and github action

--- scripts/play-browser.sh
+++ scripts/play-browser.sh
@@ -5,5 +5,5 @@ set -eu
 REPO_ROOT="$(git rev-parse --show-toplevel)"
 
 yarn sass:compile
-open $REPO_ROOT/server/index.html
+open $REPO_ROOT/public/index.html
 cd $REPO_ROOT/server/ && node index.js
