c33623470498c9b9164c4c263a42c00ebaaf37bf
declanslevin <33623842+declanslevin@users.noreply.github.com>
2021-04-14 14:26:10 +0100

    Add types to message.ts

--- public/message.ts
+++ public/message.ts
@@ -1,53 +1,102 @@
 import { updateScroll } from "./ui";
 import { gameOver } from "./game";
 
-export const handleMessage = (message) => {
+//  TODO: Does it need to be more specific than HTMLElement? i.e. HTMLParagraphElement
+export const getLogsElement = (): HTMLElement => {
+  const logs = document.getElementById("logs");
+  if (!logs) {
+    throw new Error("Unable to return 'logs' element");
+  }
+  return logs;
+};
+
+// TODO: What type to use for message - string or MessageEvent?
+export const handleMessage = (message: MessageEvent): void => {
+  // string type introduces errors
   let msg = JSON.parse(message.data);
   console.log(msg);
-  const logElement = document.getElementById("logs");
-  let currentText = logElement.innerHTML;
+  let currentText = getLogsElement().innerHTML;
   switch (msg.type) {
     case "prompt":
       if (currentText === "Logs appear here") {
         currentText = "";
       }
-      document.getElementById("logs").innerHTML =
+      getLogsElement().innerHTML =
         currentText + `<p class="log-text">${msg.prompt}</p>`;
       updateScroll();
       break;
+
     case "log":
       if (currentText === "Logs appear here") {
         currentText = "";
       }
-      document.getElementById("logs").innerHTML =
+      getLogsElement().innerHTML =
         currentText + `<p class="log-text">${msg.log}</p>`;
       updateScroll();
       break;
+
     case "thisPlayer":
-      document.getElementById("your-player").innerHTML =
-        "Your Name: " + msg.name;
-      document.getElementById("your-team").innerHTML = "Your Team: " + msg.team;
-    case "currentPlayer":
-      document.getElementById("current-player").innerHTML =
-        "Turn: " + msg.player;
-      document.getElementById("current-team").innerHTML = "Team: " + msg.team;
+      const yourPlayer = document.getElementById("your-player");
+      if (!yourPlayer) {
+        throw new Error("Unable to return 'your-player' element");
+      }
+      yourPlayer.innerHTML = `Your Name: ${msg.name}`;
+
+      const yourTeam = document.getElementById("your-team");
+      if (!yourTeam) {
+        throw new Error("Unable to return 'your-team' element");
+      }
+      yourTeam.innerHTML = `Your Team: ${msg.team}`;
       break;
+
+    case "currentPlayer":
+      const currentPlayer = document.getElementById("current-player");
+      if (!currentPlayer) {
+        throw new Error("Unable to return 'current-player' element");
+      }
+      currentPlayer.innerHTML = `Turn: ${msg.player}`;
+
+      const currentTeam = document.getElementById("current-team");
+      if (!currentTeam) {
+        throw new Error("Unable to return 'current-team' element");
+      }
+      currentTeam.innerHTML = `Team: ${msg.team}`;
+      break;
+
     case "playerChoice":
       let grid = document.getElementById(`grid-${msg.choice}`);
+      if (!grid) {
+        throw new Error(`Unable to return 'grid-${msg.choice}' element`);
+      }
       grid.innerHTML = msg.team;
       // TODO: Do this by adding a class i.e. disabled
       grid.setAttribute("style", "pointer-events:none");
+      break;
+
     case "cpuChoice":
       grid = document.getElementById(`grid-${msg.choice}`);
+      if (!grid) {
+        throw new Error(`Unable to return 'grid-${msg.choice}' element`);
+      }
       grid.innerHTML = msg.team;
       grid.setAttribute("style", "pointer-events:none");
       break;
+
     case "draw":
-      document.getElementById("game-winner").innerHTML = "You drew!";
+      let gameWinner = document.getElementById("game-winner");
+      if (!gameWinner) {
+        throw new Error("Unable to return 'game-winner' element");
+      }
+      gameWinner.innerHTML = "You drew!";
       gameOver();
       break;
+
     case "win":
-      document.getElementById("game-winner").innerHTML = `${msg.winner} won!`;
+      gameWinner = document.getElementById("game-winner");
+      if (!gameWinner) {
+        throw new Error("Unable to return 'game-winner' element");
+      }
+      gameWinner.innerHTML = `${msg.winner} won!`;
       gameOver();
       break;
   }
